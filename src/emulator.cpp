948ee78e0ef90e02fe533ececb6e6a6b2356e700
Prasun <prasun.code@gmail.com>
2020-08-31 22:27:19 +0530

    Testing build

---
+++ src/emulator.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+#include "system.h"
+
+int main(int argc, char** argv){
+
+    std::cout << "Testing!" << std::endl;
+
+    return 0;
+}
---
+++ src/system.cpp
@@ -0,0 +1,13 @@
+#include "system.h"
+
+CHIP8::CHIP8()
+{
+}
+
+CHIP8::~CHIP8()
+{
+}
+
+void CHIP8::initialise(){
+
+}
